00fc358909b5717a605f907a46c1c6a4d51854e1
Vincent Chann <vc@smartapps.fr>
2017-02-13 18:13:33 +0100

    gh-01 : Init du projet : hello world

---
+++ foo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  foo
+//
+//  Created by Vincent Chann on 13/02/2017.
+//  Copyright © 2017 Smartapps. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
